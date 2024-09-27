ca73bd73082c61114ac30435fe7ebad2c365ac5d
Roshaan Faisal <158100981+WebInventix@users.noreply.github.com>
2024-09-27 13:27:42 -0700

    fix

--- models/trailer.js
+++ models/trailer.js
@@ -1,3 +1,4 @@
+const { required } = require('joi');
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
@@ -35,11 +36,111 @@ const Trailers = mongoose.model('trailers', new Schema({
 
     },
     hitch_type: {
-        type: [String],  // Array of strings for hitch types
-        enum: ['Ball Hitch', 'Fifth Wheel', 'Gooseneck', 'Pintle Hook', 'Bumper Pull', 'Weight Distribution Hitch'],  // Enum options for hitch types
+        type: String, 
+        enum: ['Other', 'Fifth Wheel', 'Gooseneck', 'Pintle', 'Bumper Pull'],  // Enum options for hitch types
         required: true,  // Set to true if hitch_type is mandatory
-        default: []      // Default to an empty array if no hitch types are provided
+    },
+    ball_size: {
+        type: String,
+        enum:['3 inch','2-5/16 inch','2 inch','1-7/8 inch','Pintle','Fith Wheel'],
+        required: false,
+    },
+    title_status: {
+        type:String,
+        enum: ['Clean', 'Salvage', 'Restored'],
+        required: false,
+    },
+    light_plug_configuration: {
+        type:String,
+        required:false
+    },
+    trailer_dimension: {
+        type:String,
+        required:false
+    },
+    rear_door_opening_height: {
+        type:String,
+        required:false
+    },
+    payload_capacity: {
+        type:String,
+        required:false
+    },
+    axles: {
+        type:String,
+        enum:['1-2000lb','2-1x3500lb','3-1x5000lb','4-2x3500lb','5-2x5000lb','6-2x7000lb','7-2x10000lb','8-2x12000lb','9-other'],
+        required:false
+    },
+    refundable_deposit: {
+        type:String,
+        required:false
+    },
+    tag_words: {
+        type:String,
+        required:false
+    },
+    rental_delay_max: {
+        type:String,
+        required:false
+    },
+    rental_delay_min: {
+        type:String,
+        required:false
+    },
+    trailer_length: {
+        type:String,
+        required:false
+    },
+    trailer_width: {
+        type:String,
+        required:false
+    },
+    vin: {
+        type:String,
+        required:false
+    },
+    province_territory: {
+        type:String,
+        required:false
+    },
+    city: {
+        type:String,
+        required:false
+    },
+    postal_codes: {
+        type:String,
+        required:false
+    },
+    location: {
+        type:String,
+        required:false
+    },
+    daily_rate: {
+        type:String,
+        required:false
+    },
+    monthly_rate: {
+        type:String,
+        required:false
+    },
+    weekly_rate: {
+        type:String,
+        required:false
+    },
+    trailer_photos: {
+        type:[String],
+        required:false
+    },
+    approve_status: {
+        type: String,
+        enum: ['Approved', 'Pending', 'Rejected'],
+    },
+    status: {
+        type: String,
+        enum: ['Online', 'On-Rent','Offline'],
+
     }
+
 
 
           
