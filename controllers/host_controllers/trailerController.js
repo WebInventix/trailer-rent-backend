c77e91f1652a313227e3424436398de81826cad1
Roshaan Faisal <158100981+WebInventix@users.noreply.github.com>
2024-09-27 17:08:48 -0700

    fix

--- controllers/host_controllers/trailerController.js
+++ controllers/host_controllers/trailerController.js
@@ -4,10 +4,11 @@ const { Trailers } = require("../../models/trailer")
 const { v4: uuidv4 } = require('uuid');
 
 const addTrailer = async (req,res) => {
-  const  {body} = req
+  const  {body,user_id} = req
     try{
         let data = body
         data.trailer_id= `Tra-${uuidv4()}`;
+        data.host_id = user_id
         const trailer = new Trailers(data);
         const savedTrailer = await trailer.save();
         return res.status(200).json({message:'Created',trailer:savedTrailer})
