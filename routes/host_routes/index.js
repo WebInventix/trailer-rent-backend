1c5adca816ba792018754879f52fd09b549bea3d
Roshaan Faisal <158100981+WebInventix@users.noreply.github.com>
2024-09-27 17:04:58 -0700

    ipdated

--- routes/host_routes/index.js
+++ routes/host_routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const {createBank,updateBank, getBanks, updateProfile} = require("../../controllers/host_controllers/index")
+const {addTrailer} = require("../../controllers/host_controllers/trailerController")
 
 const router = express.Router();
 
@@ -7,6 +8,7 @@ router.post('/add-account', createBank)
 router.post('/update-bank-account/:id', updateBank)
 router.get('/bank-list', getBanks)
 router.post('/update-profile', updateProfile)
+router.post('/add-trailer', addTrailer)
 
 
 
