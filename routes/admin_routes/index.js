cfa8fc805e6dc6b201562632cae9dee15b90f773
Roshaan Faisal <158100981+WebInventix@users.noreply.github.com>
2024-10-03 11:35:56 -0700

    fix

--- routes/admin_routes/index.js
+++ routes/admin_routes/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { userListing, userDetailsById, approveUser, getTrailer } = require('../../controllers/admin_controllers/index');
+const { userListing, userDetailsById, approveUser, getTrailer, trailerByID, trailerByHostId } = require('../../controllers/admin_controllers/index');
 
 
 const router = express.Router();
@@ -8,6 +8,8 @@ router.get('/user-listing/:role', userListing);
 router.get('/user-detail/:id', userDetailsById);
 router.post('/user-approve',approveUser )
 router.get('/trailer-get',getTrailer)
+router.get('/trailer/:id', trailerByID)
+router.get('/trailer/host/:id', trailerByHostId)
 
 
 
