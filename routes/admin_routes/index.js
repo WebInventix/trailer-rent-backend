7891406f0edafff89128dca966293b86bf6e78f4
Roshaan Faisal <158100981+WebInventix@users.noreply.github.com>
2024-10-31 09:37:43 -0700

    update

--- routes/admin_routes/index.js
+++ routes/admin_routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { userListing, userDetailsById, approveUser, getTrailer, trailerByID, trailerByHostId } = require('../../controllers/admin_controllers/index');
-
+const adminBookings = require('../../controllers/admin_controllers/bookings')
 
 const router = express.Router();
 router.get('/user-listing', userListing);
@@ -10,6 +10,7 @@ router.post('/user-approve',approveUser )
 router.get('/trailer-get',getTrailer)
 router.get('/trailer/:id', trailerByID)
 router.get('/trailer/host/:id', trailerByHostId)
+router.get('/get-all-bookings', adminBookings.getAllBookings)
 
 
 
