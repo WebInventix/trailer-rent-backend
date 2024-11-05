7d7649ad47f81ec08ce6f571ad06fa6c33c8237e
Roshaan Faisal <158100981+WebInventix@users.noreply.github.com>
2024-11-05 11:40:57 -0800

    fix

--- routes/host_routes/index.js
+++ routes/host_routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {createBank,updateBank, getBanks, updateProfile, dashboard, updateBankStatus} = require("../../controllers/host_controllers/index")
 const {addTrailer,getTrailerById,getTrailersByStatus,editTrailer, getAllTrailers, trailerStatusUpdate} = require("../../controllers/host_controllers/trailerController")
 const myBookings = require('../../controllers/host_controllers/hostBookings')
+const tickets = require('../../controllers/host_controllers/tickets')
 
 const router = express.Router();
 
@@ -21,6 +22,7 @@ router.get('/get-my-bookings', myBookings.getMyBookings)
 router.get('/view-my-booking/:id', myBookings.viewBooking)
 router.post('/booking-reject-confirm',  myBookings.accceptRejectBooking)
 router.get('/my-chat-users',myBookings.getChatUsers)
+router.post('/add-ticket', tickets.addTicket)
 
 
 
